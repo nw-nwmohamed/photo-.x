3587ebfbefef86bb82b9585ce01e6687c119cdd0
nw-nwmohamed <mohammedhassen524@gmail.com>
2021-10-04 03:51:50 +0300

    photo .x

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+
+function add(){
+document.getElementById("div11").textContent = "welcom m.r/abd el rahman"
+};
+
+
+
+
+
+function ssd(){
+window.open("pag2.html","-blank","width=500ppx,height=500px")
+
+
+}
+
+
+
